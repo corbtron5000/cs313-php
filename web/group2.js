5fa94163f35ac3d30cf1bc04bcc5392e083c379f
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-09-25 15:46:04 -0600

    group2 assignment added

---
+++ web/group2.js
@@ -0,0 +1,11 @@
+function clickMe() {
+	// body...
+	alert("Clicked!");
+}
+
+function getColor() {
+	// body...
+	var newColor = document.getElementById("color").value;
+	document.getElementById("div1").style.backgroundColor = newColor;
+
+}
