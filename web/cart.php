de4197aa40235762a71bf5edc9dac7e87d7cdce9
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-05 11:47:49 -0600

    starting cart.php

---
+++ web/cart.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="prove3.css">
+	<title>Shopping Cart</title>
+</head>
+<body>
+	<header>
+		<h1 id="title">Puzzle Mania</h1>
+		<ul>
+			<li><a href="prove3.php">Home</a></li>
+			<li><a href="cart.php">Shopping Cart</a></li>
+			<li><a href="checkout.php">Checkout</a></li>
+			<li><a href="assignments.html">Assignments</a></li>
+		</ul>
+	</header>
+<br><br><br><br><br><br><br><br>
+</body>
+</html>
