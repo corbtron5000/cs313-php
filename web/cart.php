f9d3ddd41aca4f33a9197e09dca627488e0326c5
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-06 14:01:15 -0600

    make final changes to remove

--- web/cart.php
+++ web/cart.php
@@ -36,19 +36,22 @@
 					
 				}
 
-				$q2 = $_SESSION['count2'];
-				$price2 = $_SESSION['p2'] * $q2;
-				echo "<div>";
-				echo "<img src='c2x2.jpg' height='200' width='200'>";
-				echo "<div id='cube2>";
-				echo "<h3> 2 x 2 Rubik's Cube </h3>";
-				echo "<label> Quantity: $q2 </table?";
-				echo "<label> price: $ $price2.00 </label>";
-				echo "<br>";
-				echo "<input type='submit' name='remove2' value='remove one'>";
-				echo "<a href= 'prove3.php'> Continue shopping </a>";
-				echo "</div>";
-				echo "</div>";
+				if ($_SESSION['count2'] > 0) {
+
+					$q2 = $_SESSION['count2'];
+					$price2 = $_SESSION['p2'] * $q2;
+					echo "<div>";
+					echo "<img src='c2x2.jpg' height='200' width='200'>";
+					echo "<div id='cube2'>";
+					echo "<h3> 2 x 2 Rubik's Cube </h3>";
+					echo "<label> Quantity: $q2 </table?";
+					echo "<label> price: $ $price2.00 </label>";
+					echo "<br>";
+					echo "<input type='submit' name='remove2' value='remove one'>";
+					echo "<a href= 'prove3.php'> Continue shopping </a>";
+					echo "</div>";
+					echo "</div>";
+				}
 
 			}
 		?>
