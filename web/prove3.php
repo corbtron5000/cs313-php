2fd62ca2a8d389a518f72dec0110d5b931e9402f
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-06 00:20:51 -0600

    semi colon error

--- web/prove3.php
+++ web/prove3.php
@@ -60,7 +60,7 @@
 						function cubes2()
 						{
 							$carts = $_POST['cube2'];
-							$_SESSION["count2"] = $carts
+							$_SESSION["count2"] = $carts;
 							$_SESSION["cart"] = $_POST["cube2"];
 							echo "prove3.php";
 						}
