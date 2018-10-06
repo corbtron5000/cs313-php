7a17578957a53eb55647e15ea3b8c639e0a9be24
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-06 10:45:51 -0600

    trying to fix php error

--- web/cart.php
+++ web/cart.php
@@ -32,10 +32,10 @@
 				$q2 = $_SESSION['count2'];
 				$price2 = $_SESSION['p2'] * $q2;
 				echo "<div>";
-				echo "<img src="c2x2.jpg" height="200" width="200">";
+				echo "<img src='c2x2.jpg' height='200' width='200'>";
 				echo "<label> Quanity: $q2 </table?";
 				echo "<label> prices: $price2 </label>";
-				echo "<input type="submit" name="Add2x2" value="Add to cart">";
+				echo "<input type='submit' name='Add2x2' value='Add to cart'>";
 				echo "</div>";
 
 			}
