5c961565d964ea6a6348c38f049b364032296070
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-06 10:41:32 -0600

    trying to get 2x2 to work on cart

--- web/cart.php
+++ web/cart.php
@@ -21,15 +21,26 @@
 <main>
 	<h1 id="shopping">Shopping Cart</h1>
 
-	<?php
-		if ($_SESSION["cart"] == "0") {
-			echo "<p> cart is empty </p>";
-		}
-		else {
-			echo "<p> I am not empty </p>";
-			print_r($_SESSION);
-		}
-	?>
+	<form>
+		<?php
+			if ($_SESSION["cart"] == 0) {
+				echo "<p> cart is empty </p>";
+			}
+			
+			if ($_SESSION['count2'] > 0) {
+
+				$q2 = $_SESSION['count2'];
+				$price2 = $_SESSION['p2'] * $q2;
+				echo "<div>";
+				echo "<img src="c2x2.jpg" height="200" width="200">";
+				echo "<label> Quanity: $q2 </table?";
+				echo "<label> prices: $price2 </label>";
+				echo "<input type="submit" name="Add2x2" value="Add to cart">";
+				echo "</div>";
+
+			}
+		?>
+	</form>
 </main>
 
 </body>
