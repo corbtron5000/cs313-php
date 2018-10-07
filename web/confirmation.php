1871e2ec6f378b31abec00d50ec06de32eaaf818
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-07 03:08:18 -0600

    added css page for confirmation

---
+++ web/confirmation.php
@@ -0,0 +1,12 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="prove3.css">
+	<title>Confirmation</title>
+</head>
+<body>
+</body>
+</html>
