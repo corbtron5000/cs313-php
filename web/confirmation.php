9c06f6d955465594e09a7b190ef8bf8fc7f2817a
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-07 03:17:59 -0600

    add header to confirmation page

--- web/confirmation.php
+++ web/confirmation.php
@@ -3,10 +3,20 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<link rel="stylesheet" type="text/css" href="confirmation.css">
-	<title>Confirmation</title>
-</head>
-<body>
-</body>
+	<head>
+		<link rel="stylesheet" type="text/css" href="confirmation.css">
+		<title>Confirmation</title>
+		</head>
+	<body>
+		<header>
+		<h1 id="title">Puzzle Mania</h1>
+		<ul>
+			<li><a href="prove3.php">Home</a></li>
+			<li><a href="cart.php">Shopping Cart</a></li>
+			<li><a href="checkout.php">Checkout</a></li>
+			<li><a href="assignments.html">Assignments</a></li>
+		</ul>
+	</header>
+	<br><br><br><br><br><br><br><br>
+	</body>
 </html>
