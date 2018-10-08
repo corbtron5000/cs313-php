bd9c9d663128ea84436b5c76158ef083c7103e6a
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-08 17:12:18 -0600

    added verification

--- web/checkout.php
+++ web/checkout.php
@@ -3,6 +3,18 @@
 ?>
 <!DOCTYPE html>
 <html>
+	<script>
+		function verify() {
+			var address = getElementById('address').value;
+			var city = getElementById('city').value;
+			var zip = getElementById('Zip').value;
+			var state = getElementById('state').value;
+
+			if (address == "" || city == "" || zip == "" || state == "") {
+				return false;
+			}
+		}
+	</script>
 <head>
 	<link rel="stylesheet" type="text/css" href="checkout.css">
 	<title>Checkout</title>
@@ -22,10 +34,10 @@
 		<br><br><br><br><br><br><br><br>
 		<h1>Checkout</h1>
 		<form method="POST", action="confirmation.php">
-			<label>Address </label><input type="text" name="address" placeholder="123 Main St."><br>
-			<label>City </label><input type="text" name="city" placeholder="Rexburg"><br>
-			<label>Zip Code </label><input type="text" name="zip" placeholder="11111"><br>
-			<label>State </label><input type="text" name="state" placeholder="CA"><br>
+			<label>Address </label><input type="text" name="address" id="address" placeholder="123 Main St."><br>
+			<label>City </label><input type="text" name="city" id="city" placeholder="Rexburg"><br>
+			<label>Zip Code </label><input type="text" name="zip" id="zip" placeholder="11111"><br>
+			<label>State </label><input type="text" name="state" id="state" placeholder="CA"><br>
 			<input type="submit" name="submit" value="place order">
 			<a href="cart.php">Return to cart</a>
 		</form>
