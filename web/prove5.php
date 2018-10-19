c1c67d7fc9664f9a23d5b6efbd67d83f07556af5
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-18 19:55:30 -0600

    fixed like to mission page

--- web/prove5.php
+++ web/prove5.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>International Spies</title>
 </head>
 <body>
 	<header>
-		<h1 id="title">Internationl Spy Co.</h1>
+		<h1 id="title">International Spy Co.</h1>
 		<ul>
 			<li><a href="prove5.php">Home</a></li>
-			<li><a href="cart.php">MissionsCart</a></li>
+			<li><a href="missions.php">Missions</a></li>
 			<li><a href="assignments.html">Assignments</a></li>
 		</ul>
 	</header>
 </body>
+</html>
