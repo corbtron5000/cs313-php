74720150dd6e7293a9742ecd4cafa18f546da086
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-19 11:44:43 -0600

    trying to display data from table.

--- web/prove5.php
+++ web/prove5.php
@@ -16,8 +16,46 @@
 	<main>
 		<br><br><br><br><br><br><br>
 		<form>
-			<h1>List of Spys</h1>
+			<h1>List of Spyies</h1>
+			<?php
 
+			$dbUrl = getenv('DATABASE_URL');
+			$dbOpts = parse_url($dbUrl);
+			$dbHost = $dbOpts["host"];
+			$dbPort = $dbOpts["port"];
+			$dbUser = $dbOpts["user"];
+			$dbPassword = $dbOpts["pass"];
+			$dbName = ltrim($dbOpts["path"],'/');
+
+			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch (PDOException $ex)
+			{
+				echo 'Error!: ' . $ex->getMessage();
+			  	die();
+			}
+
+			$stmt = $db->prepare('SELECT id, name, image, description FROM spys');
+     		$stmt->execute();
+     		$rows = $stmt->fetch(PDO::FETCH_ASSOC);
+
+     		foreach ($rows as $row) {
+     			$id = $row['id'];
+     			$name = $row['name'];
+     			$image = $row['image'];
+     			$description = $row['description'];
+
+     			echo "<div id='spy'>";
+     			echo "<img src='" . "$image height='200' width='200'";
+     			echo "<div id='text>";
+     			echo "<h3> $name </h3>";
+     			echo "<p> Description </P>";
+     			echo "<p> $description </p>";
+     			echo "</div></div>";
+     		}
+			?>
 		</form>
 	</main>
 </body>
