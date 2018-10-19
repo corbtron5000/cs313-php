544ca3b7669d9f90a97f6fc95739ca8609e80122
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-19 17:10:39 -0600

    changed rows to row

--- web/missions.php
+++ web/missions.php
@@ -50,7 +50,7 @@
       			$stmt = $db->prepare('SELECT id, name, image, description, time, location, contact_info FROM spys WHERE id=:id');
       			$stmt->bindValue(':id', $id, PDO::PARAM_INT);
       			$stmt->execute();
-      			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      			$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
       			$name = $row['name'];
      			$image = $row['image'];
