bd40cb0a1025696d3112ad0a93f552df8d05cbdd
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-19 15:31:15 -0600

    seeing if php file is working

--- web/missions.php
+++ web/missions.php
@@ -15,6 +15,37 @@
 	</header>
 	<main>
 		<br><br><br><br><br><br><br>
+		<h1>Spy</h1>
+		<?php
+			function getId($data) {
+
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+		try {
+				$dbUrl = getenv('DATABASE_URL');
+				$dbOpts = parse_url($dbUrl);
+				$dbHost = $dbOpts["host"];
+				$dbPort = $dbOpts["port"];
+				$dbUser = $dbOpts["user"];
+				$dbPassword = $dbOpts["pass"];
+				$dbName = ltrim($dbOpts["path"],'/');
+				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch (PDOException $ex)
+			{
+				echo 'Error!: ' . $ex->getMessage();
+			  	die();
+			}
+
+			$stmt = $db->prepare('SELECT id, name, image, description FROM spys');
+     		$stmt->execute();
+     		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		?>
 	</main>
 </body>
 </html>
