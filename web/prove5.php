f03ffd68b40da189dded308787c916ee0e2afdcc
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-19 13:34:17 -0600

    fixing css

--- web/prove5.php
+++ web/prove5.php
@@ -8,9 +8,9 @@
 	<header>
 		<h1 id="title">International Spy Co.</h1>
 		<ul>
-			<li><a href="prove5.php">Home</a></li>
-			<li><a href="missions.php">Missions</a></li>
-			<li><a href="assignments.html">Assignments</a></li>
+			<li><a href="prove5.php" class='link'>Home</a></li>
+			<li><a href="missions.php" class='link'>Missions</a></li>
+			<li><a href="assignments.html" class='link'>Assignments</a></li>
 		</ul>
 	</header>
 	<main>
