24dccc39cab714e514861b18c6e956440923107f
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-18 19:47:43 -0600

    starting prove5.php

---
+++ web/prove5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<header>
+		<h1 id="title">Internationl Spy Co.</h1>
+		<ul>
+			<li><a href="prove5.php">Home</a></li>
+			<li><a href="cart.php">MissionsCart</a></li>
+			<li><a href="assignments.html">Assignments</a></li>
+		</ul>
+	</header>
+</body>
