a19267f2f4520d268af150542666dca2d3a3fb64
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-20 17:25:35 -0600

    adding add spy page

--- web/missions.php
+++ web/missions.php
@@ -10,6 +10,7 @@
 		<ul>
 			<li><a href="prove5.php" class='link'>Home</a></li>
 			<li><a href="missions.php" class='link'>Missions</a></li>
+			<li><a href="addSpy.php" class='link'>Add Spy</a></li>
 			<li><a href="assignments.html" class='link'>Assignments</a></li>
 		</ul>
 	</header>
@@ -63,7 +64,7 @@
       			echo "<img src='" . "$image' height='200' width='200'>";
       			echo "<p><span> Description: </span>$description</p>";
       			echo "<p><span> Location: </span>$location</p>";
-      			echo "<p><span> Length of Employment: </span>$year years</p>";
+      			echo "<p><span> Length of Employment: </span>$year</p>";
       			echo "<p><span> Contact Info: </span>$contact</p>";
 
       		}
--- web/prove5.php
+++ web/prove5.php
@@ -10,6 +10,7 @@
 		<ul>
 			<li><a href="prove5.php" class='link'>Home</a></li>
 			<li><a href="missions.php" class='link'>Missions</a></li>
+			<li><a href="addSpy.php" class='link'>Add Spy</a></li>
 			<li><a href="assignments.html" class='link'>Assignments</a></li>
 		</ul>
 	</header>
