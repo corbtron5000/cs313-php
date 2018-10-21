fbdd3035e2ec2d0ab63dc4509f36ad18858326ac
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-20 20:04:37 -0600

    removed a navigation item

--- web/addSpy.php
+++ web/addSpy.php
@@ -10,7 +10,6 @@
 		<h1 id="title">International Spy Co.</h1>
 		<ul>
 			<li><a href="prove5.php" class='link'>Home</a></li>
-			<li><a href="missions.php" class='link'>Missions</a></li>
 			<li><a href="addSpy.php" class='link'>Add Spy</a></li>
  			<li><a href="assignments.html" class='link'>Assignments</a></li>
 		</ul>
