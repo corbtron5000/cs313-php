4cafa293377b167b0dccf69c8feac5cceee9973d
Corbin Lenning <corbin@205-185-99-159.directcom.com>
2018-10-20 19:56:49 -0600

    fixing center

--- web/addSpy.php
+++ web/addSpy.php
@@ -17,15 +17,17 @@
 	</header>
 	<main>
 		<br><br><br><br><br><br><br>
-		<h1>Adding New Agent</h1>
+		<h1 id="title">Adding New Agent</h1>
 		<form>
-			<label>Name: </label><input type="text" placeholder="Enter Name" name="name"> <br>
-			<label>Location: </label><input type="text" name="location" placeholder="Enter Location"><br>
-			<label>Alias: </label><input type="text" name="Alias" placeholder="Enter Alias"><br>
-			<label>Contact Info:</label><input type="text" name="contact" placeholder="Enter Contact"><br>
-			<label>Years of service: </label><input type="text" name="service" placeholder="Enter years"><br>
-			<label>Description: </label><input type="text" name=""><br>
-			<input type="submit" name="submit" value="Add Agent">
+			<div id="form">
+				<label>Name: </label><input type="text" placeholder="Enter Name" name="name"> <br>
+				<label>Location: </label><input type="text" name="location" placeholder="Enter Location"><br>
+				<label>Alias: </label><input type="text" name="Alias" placeholder="Enter Alias"><br>
+				<label>Contact Info:</label><input type="text" name="contact" placeholder="Enter Contact"><br>
+				<label>Years of service: </label><input type="text" name="service" placeholder="Enter years"><br>
+				<label>Description: </label><input type="text" name=""><br>
+				<input type="submit" name="submit" value="Add Agent">
+			</div>
 		</form>
 	</main>
 
